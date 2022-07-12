0eeb9806b49e4b8b724c0b4949ae205d909fa92a
Alex Boyko <alexboyko@outlook.com>
2022-07-11 21:46:23 -0700

    Prototype API functions

--- api/list.js
+++ api/list.js
@@ -1,18 +1,30 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import { loadProject } from "./redis/client.js";
+import 
+{ loadList, 
+  loadListItem
+} from "./redis/client.js";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Defines path to where data is sent
-app.get("/api/projects", async (req, res) => {
+app.get("/api/list", async (req, res) => {
   try {
-    const project = await loadProject();
+    const items = [];
+
+    let i = 0;
+    do {
+      const listItem = await loadListItem(i);
+      items.push(listItem);
+      i++;
+    } while (i < 2);
+    
     res.header("Content-Type", "application/json");
-    res.send(JSON.stringify(project));
+    res.send(items);
+
   } catch (error) {
     logErrorMessage(userInfo, error);
     res.sendStatus(500);
--- api/redis/client.js
+++ api/redis/client.js
@@ -7,12 +7,32 @@ export const client = redis.createClient();
 export const jsonsetAsync = promisify(client.json_set).bind(client);
 export const jsongetAsync = promisify(client.json_get).bind(client);
 
+export const smembersAsync = promisify(client.smembers).bind(client);
+export const saddAsync = promisify(client.sadd).bind(client);
+export const sremAsync = promisify(client.srem).bind(client);
+
+export const zaddAsync = promisify(client.zadd).bind(client);
+export const zcardAsync = promisify(client.zcard).bind(client);
+export const zincrbyAsync = promisify(client.zincrby).bind(client);
+export const zremAsync = promisify(client.zrem).bind(client);
+export const zscoreAsync = promisify(client.zscore).bind(client);
+export const zrevrangeAsync = promisify(client.zrevrange).bind(client);
+export const zrangeAsync = promisify(client.zrevrange).bind(client);
+
 export async function loadProject() {
   const project = await jsongetAsync('card');
 
   return project;
 }
 
-export async function loadProjects() {
-  // const projects = await 
+// Load item from a Redis list,
+// then get the associated JSON and return it
+export async function loadList() {
+  const list = await zrevrangeAsync('cards', 0, -1);
+  return list;
+}
+
+export async function loadListItem(index) {
+  const listItem = await zrangeAsync('cards', index, index);
+  return listItem;
 }
