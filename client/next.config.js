13ea2944be2348c2a6f5cfb26f0fd12c1490f515
Alex Boyko <alexboyko@outlook.com>
2022-07-26 22:36:37 -0700

    Add custom styles via scss

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
